f618c28b69bd8575162287e1961f5b9029f7487b
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2024-12-12 11:26:44 +0900

    page.svelteの変更

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-    export const prerender = true;
-    export const ssr = true;
     export let Tasks = [];
     export let task = '';
     export let num=1;
@@ -57,41 +55,128 @@
     function deltask(index){
         Tasks = Tasks.filter((_, i) => i !== index);
     }
-
 </script>
 
 <center>
-    <h1>Todoリスト</h1>
-    <input type ="text" bind:value={task} placeholder="Taskname" />
-    <input type="number" bind:value={num} min="1" max="5"/>
-    <br>
-    <button on:click={addtask}>作成</button>
+    <div class  = "all-container">
+        <center>
+            <h1>Todoリスト</h1>
+            <input type ="text" bind:value={task} placeholder="Taskname" />
+            <input type="number" bind:value={num} min="1" max="5"/>
+            <button on:click={addtask}>作成</button>
+        </center>
+    </div>
+        <br>
+        {#if Tasks.length > 0}
+            {#each Tasks as {task,num},i}
+                {#if num == 1}
+                    <div class = "task-container">
+                        <div class = "task1">
+                        <p>{task}</p>
+                            <button on:click={()=>deltask(i)}>完了</button>
+                        </div>
+                    </div>
+                {:else if num == 2}
+                    <div class = "task-container">
+                        <div class = "task2">
+                        <p>{task}</p>
+                            <button on:click={()=>deltask(i)}>完了</button>
+                        </div>
+                    </div>
+                {:else if num == 3}
+                    <div class = "task-container">
+                        <div class = "task3">
+                        <p>{task}</p>
+                            <button on:click={()=>deltask(i)}>完了</button>
+                        </div>
+                    </div>
+                {:else if num == 4}
+                    <div class = "task-container">
+                        <div class = "task4">
+                        <p>{task}</p>
+                            <button on:click={()=>deltask(i)}>完了</button>
+                        </div>
+                    </div>
+                {:else}
+                    <div class = "task-container">
+                        <div class = "task5">
+                        <p>{task}</p>
+                            <button on:click={()=>deltask(i)}>完了</button>
+                        </div>
+                    </div>
+                {/if}
+            {/each}<!--checkboxの値変更で，その要素をdel-->
+        {/if}
 </center>
-<br>
-{#if Tasks.length > 0}
-    {#each Tasks as {task},i}
-        <div class = "task-container">
-            <div class = "task">
-            <p>{task}</p>
-                <button on:click={()=>deltask(i)}>完了</button>
-            </div>
-        </div>
-    {/each}<!--checkboxの値変更で，その要素をdel-->
-{/if}
-
 <style>
-
+    .all-container{
+        background-color: #ffffff;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        width: 600px;
+        align-items: center;
+        justify-content:center;
+        padding-bottom: 30px;
+        padding-top:10px;
+    }
     .task-container {
         display: flex;
         justify-content: center;
-        padding: 2px;
+        padding: 5px;
     }
 
-    .task{
+    .task1{
         display: flex;
         align-items: center;
         justify-content: space-between;
-        background-color: #f9f9f9;
+        background-color: #1ce064;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        padding-left:30px;
+        padding-right:30px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        width:250px;
+    }
+    .task2{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #81c422;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        padding-left:30px;
+        padding-right:30px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        width:250px;
+    }
+    .task3{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #e7e745;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        padding-left:30px;
+        padding-right:30px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        width:250px;
+    }
+    .task4{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #e08d4a;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        padding-left:30px;
+        padding-right:30px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        width:250px;
+    }
+    .task5{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #cb4848;
         border: 1px solid #ddd;
         border-radius: 8px;
         padding-left:30px;
