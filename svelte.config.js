d0754b57478eef7626ca7897954880cea5506c85
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2024-12-12 12:09:10 +0900

    svelteconfigの変更

--- svelte.config.js
+++ svelte.config.js
@@ -3,25 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // Use the static adapter and specify the output directory as 'build'
+    adapter: adapter({
+      out: 'build'
+    }),
+
+    prerender: {
+      entries: ['*'] // Prerender all pages
+    }
+  }
 };
 
-export default {
-	kit: {
-	  adapter: adapter({
-		out: 'build' // 出力先を 'build' に指定
-	  }),
-	  prerender: {
-		entries: ['*']
-	  },
-	},
-  };
+export default config;
